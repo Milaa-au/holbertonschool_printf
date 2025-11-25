e5cf2c12364ce34e76a3af170a0b7b3f7f716455
Milaa-au <mila.audu@icloud.com>
2025-11-25 13:41:48 +0000

    Ajout de la bibliothèque standard

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+
+int _printf(const char *format, ...);
+
+typedef struct funcs
+{
+    char c;
+    int (*f)(va_list);
+} funcs_t;
+
+int get_func(char s, va_list ap);
+int print_percent(va_list ap);
+int print_char(va_list ap);
+int print_string(va_list ap);
+int print_int(va_list ap);
+
+#endif /* MAIN_H */
