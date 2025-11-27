dc6c93e0e80023fdcb2e660f341cd520721c2ca8
Milaa-au <mila.audu@icloud.com>
2025-11-27 08:24:20 +0000

    Ajout des commentaires Betty et correction des erreurs Betty.

--- handlers.c
+++ handlers.c
@@ -2,70 +2,107 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * print_char - Imprime un caractère
+ * @ap: Liste d'arguments variadiques contenant le caractère à imprimer
+ *
+ * Return: Toujours 1, correspondant au nombre de caractères imprimés.
+ */
+
 int print_char(va_list ap)
 {
-    char c = (char)va_arg(ap, int);
-    putchar(c);
-    return 1;
+	char c = (char)va_arg(ap, int);
+
+	putchar (c);
+	return (1);
 }
+
+/**
+ * print_percent - Imprime le caractère '%'
+ * @ap: Liste d'arguments variadiques (non utilisée)
+ *
+ * Return: Toujours 1, correspondant au nombre de caractères imprimés.
+ */
 
 int print_percent(va_list ap)
 {
-    (void)ap;
-    putchar('%');
-    return 1;
+	(void)ap;
+	putchar('%');
+	return (1);
 }
+
+/**
+ * print_string - Imprime une chaîne de caractères
+ * @ap: Liste d'arguments variadiques contenant la chaîne à imprimer
+ *
+ * Description: Si la chaîne reçue est NULL, la fonction imprime "(null)".
+ * La fonction parcourt ensuite la chaîne et imprime chaque caractère.
+ *
+ * Return: Le nombre total de caractères imprimés.
+ */
 
 int print_string(va_list ap)
 {
-    char *str = va_arg(ap, char *);
-    int len = 0;
+	char *str = va_arg(ap, char *);
+	int len = 0;
 
-    if (str == NULL)
-    {
-        str = "(null)";
-    }
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
 
-    while (str[len] != '\0')
-    {
-        putchar(str[len]);
-        len++;
-    }
-    
-    return (len);
+	while (str[len] != '\0')
+	{
+		putchar(str[len]);
+		len++;
+	}
+
+	return (len);
 }
+
+/**
+ * print_int - Imprime un entier sous forme décimale
+ * @ap: Liste d'arguments variadiques contenant l'entier à imprimer
+ *
+ * Description: La fonction gère les entiers négatifs, imprime un tiret
+ * si nécessaire, puis convertit le nombre en chaîne en stockant chaque
+ * chiffre dans un buffer temporaire avant de les afficher dans l'ordre
+ * correct.
+ *
+ * Return: Le nombre total de caractères imprimés.
+ */
 
 int print_int(va_list ap)
 {
-    int integer = va_arg(ap, int);
-    char len[12];
-    int i = 0, j, number = 0;
-    unsigned int num;
+	int integer = va_arg(ap, int);
+	char len[12];
+	int i = 0, j, number = 0;
+	unsigned int num;
 
-    if (integer < 0)
-    {
-        putchar('-');
-        number++;
-        num = -integer;
-    }
-    else
-    {
-        num = integer;
-    }
+	if (integer < 0)
+	{
+		putchar('-');
+		number++;
+		num = -integer;
+	}
+	else
+	{
+		num = integer;
+	}
 
-    if (num == 0)
-    {
-        len[i++] = '0';
-    }
-    while (num > 0)
-    {
-        len[i++] = (num % 10) + '0';
-        num /= 10;
-    }
-    for (j = i - 1; j >= 0; j--)
-    {
-        putchar(len[j]);
-        number++;
-    }
-    return (number);
+	if (num == 0)
+	{
+		len[i++] = '0';
+	}
+	while (num > 0)
+	{
+		len[i++] = (num % 10) + '0';
+		num /= 10;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		putchar(len[j]);
+		number++;
+	}
+	return (number);
 }
--- structures.c
+++ structures.c
@@ -2,25 +2,41 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * get_func - Sélectionne et exécute la fonction correspondant à un spéci
+ * @s: Caractère représentant le spécificateur de format
+ * @ap: Liste d’arguments variadiques à transmettre à la fonction trouvée
+ *
+ * Description: Cette fonction parcourt un tableau de structures contenant
+ * les spécificateurs pris en charge et les fonctions associées. Lorsqu’un
+ * spécificateur correspond à celui fourni en argument, la fonction associée
+ * est appelée avec les arguments variadiques.
+ *
+ * Return: Le résultat retourné par la fonction trouvée. Retourne -1 si
+ * aucun spécificateur correspondant n’est trouvé.
+ */
+
 int get_func(char s, va_list ap)
 {
-    int i = 0;
+	int i = 0;
 
-    funcs_t funcs[] = {
-        {'c', print_char},
-        {'s', print_string},
-        {'%', print_percent},
-        {'d', print_int},
-        {'i', print_int},
-        {'\0', NULL}
-    };
+	funcs_t funcs[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'d', print_int},
+		{'i', print_int},
+		{'\0', NULL}
+	};
 
-    while (funcs[i].c)
-    {
-        if (funcs[i].c == s)
-            return funcs[i].f(ap);
-        i++;
-    }
+	while (funcs[i].c)
+	{
+		if (funcs[i].c == s)
+		{
+			return (funcs[i].f(ap));
+		}
+		i++;
+	}
 
-    return -1;
+	return (-1);
 }
